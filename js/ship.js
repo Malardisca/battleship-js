8e0019907ae9b801b8a12e3ce86e08cc8bc9e761
Shayen Patel <s775p583@ku.edu>
2021-02-17 10:44:23 -0600

    Made a reveal square funtion to add hits to each type of ship and store e

--- js/ship.js
+++ js/ship.js
@@ -4,7 +4,32 @@ class Ship
     function playGame() {
         if(currentPlayer === 'playerOne')
         {
-            
+            turnDisplay.innerHTML = 'Player Ones turn'
+            playerTwo.forEach(square => square.addEventListerner('click', function(e) {
+                revealSquare(square)
+            }))
         }
+        if(currentPlayer === 'playerTwo')
+        {
+            turnDisplay.innerHTML = 'Player Twos turn'
+            playerOne.forEach(square => square.addEventListener('click',function(e) {
+                revealSquare(square)
+            } ))
+        }
+    }
+    let destroyerCount = 0
+    let submarineCount = 0
+    let cruiserCount = 0
+    let battleshipCount = 0
+    let carrierCount = 0
+
+    function revealSquare(square) {
+        if(square.classList.contains('destroyer')) destroyerCount++
+        if(square.classList.contains('submarine')) submarineCount++
+        if(square.classList.contains('cruiser')) submarineCount++
+        if(square.classList.contains('battleship')) battleshipCount++
+        if(square.classList.contains('carrier')) carrierCount++
+
+        if(squares)
     }
 };
